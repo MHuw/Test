1b406eee816f10016210d7fd9dabfb4c787eef4b
macse <macse@DESKTOP-0MK86PA.home>
2019-11-06 20:19:00 +0000

    test

---
+++ src/ultimate/Ultimate.java
@@ -0,0 +1,15 @@
+package ultimate;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Ultimate {
+
+ public static void main(String[] args) {
+    MainMenu form = new MainMenu();
+    form.setVisible(true);
+}
+     
+}
+         
+         
